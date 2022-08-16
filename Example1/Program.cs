a1cfc7f94fcd15b6ba78f8514d9da0c47574eb7a
Luba <ponochka.85@bk.com>
2022-08-16 12:35:49 +0300

    add Example 1

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -23,31 +23,3 @@ class Program
 
 
 
-Console.Write("Введите число: ");
-int cube = Convert.ToInt32(Console.ReadLine());
-
-void Cube(int[] cube)
-{
-    int i = 0;
-    int length = cube.Length;
-    while (i < length)
-    {
-        cube[i] = Convert.ToInt32(Math.Pow(i, 3));
-        i++;
-    }
-}
-
-void PrintArry(int[] coll)
-{
-    int count = coll.Length;
-    int index = 1;
-    while (index < count)
-    {
-        Console.Write(coll[index] + " ");
-        index++;
-    }
-}
-
-int[] arry = new int[cube + 1];
-Cube(arry);
-PrintArry(arry);
